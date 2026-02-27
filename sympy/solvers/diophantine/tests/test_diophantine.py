c101b727d25b3900bc7d8b2a4732b8963cbe982f
IronwallxR5 <rathipadamr5@gmail.com>
2026-02-28 01:07:06 +0530

    updated tests

--- sympy/solvers/diophantine/tests/test_diophantine.py
+++ sympy/solvers/diophantine/tests/test_diophantine.py
@@ -979,7 +979,8 @@ def test_issue_9538():
 
 
 def test_issue_29269():
-    assert diophantine(x + y + z - 3, syms=[x, y])
+    assert diophantine(x + y + z - 3, syms=[x, y]) == {(t_0, t_0 + t_1)}
+    assert diophantine(x + y + z - 3, syms=[y, x]) == {(t_0 + t_1, t_0)}
 
 
 def test_ternary_quadratic():
